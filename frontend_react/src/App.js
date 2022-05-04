b069261e167feba84a4b922ca27466872c2072da
Angel Cai <angelcai1818@gmail.com>
2022-05-04 03:33:10 -0600

    adding assets, components, constants, and container files

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import './App.scss'
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonial />
+        <Footer />
+    </div>
   )
 }
 
-export default App
+export default App;
